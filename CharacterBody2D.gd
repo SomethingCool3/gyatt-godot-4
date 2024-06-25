ba10bc82a8253df691486e40e2f73e0b80bdea80
SomethingCool3 <damisobanjo@outlook.com>
2024-06-25 10:29:19 -0400

    Fixed Movement

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,18 +1,18 @@
 extends CharacterBody2D
 
-@export var accelaration: float = 2.0
-@export var speed: float = 300.0
+const SPEED = 100.0
+const ACCEL = 10.0
 
 var input: Vector2
-# Called when the node enters the scene tree for the first time.
-func _physics_process(delta):
-	var playerInput = get_input()
-	
-	velocity = lerp(velocity, playerInput * speed, delta * accelaration)
-	
-	move_and_slide()
-	
+
 func get_input():
 	input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	return input.normalized()
+
+func _process(delta):
+	var playerInput = get_input()
+	
+	velocity = lerp(velocity, playerInput * SPEED, delta * ACCEL)
+	
+	move_and_slide()
